6ebecadde6fa019370250307c039658b7ae25a85
bl-nkd-v <blankthedev@gmail.com>
2024-09-16 19:51:34 -0700

    Add block checking API and hunger updating API as quickfix for deployed vercel environment

---
+++ app/api/check-new-blocks/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server";
+import {
+  client,
+  createBlock,
+  getLatestBlockReward,
+} from "@/app/services/bitcoin";
+import prisma from "@/app/services/database";
+
+export async function GET() {
+  try {
+    const blockchainInfo = await client.getBlockchainInfo();
+    const currentBlockHash = blockchainInfo.bestblockhash;
+
+    const lastKnownBlock = await prisma.bitcoinBlock.findFirst({
+      orderBy: { height: "desc" },
+    });
+
+    if (!lastKnownBlock || lastKnownBlock.id !== currentBlockHash) {
+      const latestBlockHash = await client.getBlockHash(blockchainInfo.blocks);
+      const block = await client.getBlock(latestBlockHash, 2);
+      const reward = getLatestBlockReward(block);
+      await createBlock(latestBlockHash, blockchainInfo.blocks, block);
+
+      const newEggs = Math.floor(reward * 10);
+      await prisma.pet.updateMany({
+        data: {
+          availableEggs: { increment: newEggs },
+        },
+      });
+
+      return NextResponse.json({
+        message: "New block processed",
+        blockHeight: blockchainInfo.blocks,
+      });
+    }
+
+    return NextResponse.json({ message: "No new blocks" });
+  } catch (error) {
+    console.error("Error checking for new blocks:", error);
+    return NextResponse.json(
+      { error: "Failed to check for new blocks" },
+      { status: 500 }
+    );
+  }
+}
+
+export const dynamic = "force-dynamic";
---
+++ app/api/update-hunger/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/services/database";
+
+// TODO: Add auth
+// Done as a quick fix because the hunger is not updating on deployed environment (misunderstood how nextjs works)
+export async function POST() {
+  try {
+    await prisma.pet.updateMany({
+      data: {
+        hunger: {
+          increment: 1,
+        },
+      },
+      where: {
+        hunger: {
+          lt: 100,
+        },
+      },
+    });
+
+    return NextResponse.json({ message: "Hunger updated for all pets" });
+  } catch (error) {
+    console.error("Error updating hunger:", error);
+    return NextResponse.json(
+      { error: "Failed to update hunger" },
+      { status: 500 }
+    );
+  }
+}
+
+export const dynamic = "force-dynamic";
