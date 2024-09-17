0287d5c017842b2babe6df877063213406c2b8fb
bl-nkd-v <blankthedev@gmail.com>
2024-09-16 20:20:55 -0700

    Add vercel cron jobs, remove instrumenting

--- app/api/sync-blocks/route.ts
+++ app/api/sync-blocks/route.ts
@@ -1,50 +1,57 @@
 import { NextResponse } from "next/server";
-import {
-  client,
-  createBlock,
-  getLatestBlockReward,
-} from "@/app/services/bitcoin";
+import { client, createBlock } from "@/app/services/bitcoin";
 import prisma from "@/app/services/database";
 
 // TODO: Add auth
 // Done as a quick fix because the eggs/blocks are not updating on deployed environment (misunderstood how nextjs works)
-export async function GET() {
+export const GET = async () => {
   try {
     const blockchainInfo = await client.getBlockchainInfo();
-    const currentBlockHash = blockchainInfo.bestblockhash;
+    const currentHeight = blockchainInfo.blocks;
+    const startHeight = Math.max(currentHeight - 3, 0);
 
-    const lastKnownBlock = await prisma.bitcoinBlock.findFirst({
-      orderBy: { height: "desc" },
-    });
+    const processedBlocks = [];
 
-    if (!lastKnownBlock || lastKnownBlock.id !== currentBlockHash) {
-      const latestBlockHash = await client.getBlockHash(blockchainInfo.blocks);
-      const block = await client.getBlock(latestBlockHash, 2);
-      const reward = getLatestBlockReward(block);
-      await createBlock(latestBlockHash, blockchainInfo.blocks, block);
+    for (let height = startHeight; height <= currentHeight; height++) {
+      const blockHash = await client.getBlockHash(height);
+      const block = await client.getBlock(blockHash, 2);
 
-      const newEggs = Math.floor(reward * 10);
-      await prisma.pet.updateMany({
-        data: {
-          availableEggs: { increment: newEggs },
-        },
+      const existingBlock = await prisma.bitcoinBlock.findUnique({
+        where: { id: blockHash },
       });
 
-      return NextResponse.json({
-        message: "New block processed",
-        blockHeight: blockchainInfo.blocks,
-        reward,
-      });
+      if (!existingBlock) {
+        await createBlock(blockHash, height, block);
+        processedBlocks.push(height);
+
+        const blockReward = block.tx[0].vout.reduce(
+          (sum, output) => sum + output.value,
+          0
+        );
+        const newEggs = Math.floor(blockReward * 10);
+        await prisma.pet.updateMany({
+          data: {
+            availableEggs: { increment: newEggs },
+          },
+        });
+      }
     }
 
-    return NextResponse.json({ message: "No new blocks" });
+    if (processedBlocks.length > 0) {
+      return NextResponse.json({
+        message: "Blockchain synced",
+        processedBlocks: processedBlocks,
+      });
+    } else {
+      return NextResponse.json({ message: "No new blocks to process" });
+    }
   } catch (error) {
-    console.error("Error checking for new blocks:", error);
+    console.error("Error syncing blockchain:", error);
     return NextResponse.json(
-      { error: "Failed to check for new blocks" },
+      { error: "Failed to sync blockchain" },
       { status: 500 }
     );
   }
-}
+};
 
 export const dynamic = "force-dynamic";
